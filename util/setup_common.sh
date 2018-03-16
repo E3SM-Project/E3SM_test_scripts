6f98615ff2138ef06b77e6f7913083bb7358d08f
James Foucar <jgfouca@sandia.gov>
2018-03-16 16:29:19 -0600

    Bugfig

--- util/setup_common.sh
+++ util/setup_common.sh
@@ -5,7 +5,7 @@ if [ -z "$DEBUG" ]; then
 else
     export RUNSCRIPT="echo $E3SMREPO/cime/scripts/Tools/jenkins_script"
 fi
-chmod -R a+rX E3SMREPO
+chmod -R a+rX $E3SMREPO
 
 machine_custom_setup=$SCRIPTROOT/util/${CIME_MACHINE}_setup.sh
 if [ -e $machine_custom_setup ]; then
