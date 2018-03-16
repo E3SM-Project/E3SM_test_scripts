e2091e5a7ea2ad24c1fb50905bef4a48c1ac8f0b
James Foucar <jgfouca@sandia.gov>
2018-03-16 15:13:53 -0600

    Add debug mode

--- util/setup_common.sh
+++ util/setup_common.sh
@@ -1,9 +1,13 @@
 umask 002
 export E3SMREPO=$SCRIPTROOT/../ACME
-export RUNSCRIPT=$E3SMREPO/cime/scripts/Tools/jenkins_script
+if [ -z "$DEBUG" ]; then
+    export RUNSCRIPT=$E3SMREPO/cime/scripts/Tools/jenkins_script
+else
+    export RUNSCRIPT="echo $E3SMREPO/cime/scripts/Tools/jenkins_script"
+fi
 chmod -R a+rX E3SMREPO
 
 machine_custom_setup=$SCRIPTROOT/util/${CIME_MACHINE}_setup.sh
-if [ -f $machine_custom_setup ]; then
+if [ -e $machine_custom_setup ]; then
     source $machine_custom_setup
 fi
