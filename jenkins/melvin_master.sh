efee78e7f1ed66b7731debbc1f57cf10c4667373
James Foucar <jgfouca@sandia.gov>
2018-03-16 16:08:24 -0600

    Add melvin master

---
+++ jenkins/melvin_master.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=melvin
+source $SCRIPTROOT/utils/setup_common.sh
+
+$RUNSCRIPT
---
+++ util/melvin_setup.sh
@@ -0,0 +1,10 @@
+
+source /projects/sems/modulefiles/utils/sems-modules-init.sh
+module load sems-env sems-python/2.7.9 sems-git
+
+export http_proxy=http://sonproxy.sandia.gov:80
+export RSYNC_PROXY=sonproxy.sandia.gov:80
+export rsync_proxy=sonproxy.sandia.gov:80
+export HTTPS_PROXY=http://sonproxy.sandia.gov:80
+export https_proxy=http://sonproxy.sandia.gov:80
+export HTTP_PROXY=http://sonproxy.sandia.gov:80
