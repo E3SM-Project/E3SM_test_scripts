91ca9f7f43808f14e847973199c938de7167fc4f
James Foucar <jgfouca@sandia.gov>
2018-03-23 11:41:53 -0600

    Add cime integration test to this repo

---
+++ jenkins/anvil_cime_integration.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=anvil
+source $SCRIPTROOT/util/setup_common.sh
+
+cd $E3SMREPO
+/bin/rm -rf cime
+git clone https://github.com/ESMCI/cime.git
+
+/bin/rm -rf /lcrc/group/acme/acmetest/acme_scratch/*cime_test*
+
+./cime/scripts/create_test --machine anvil PET_Ln9.ne30_oECv3_ICG.A_WCYCL1850S -t cime_test
+
+./cime/scripts/Tools/wait_for_tests /lcrc/group/acme/acmetest/acme_scratch/*cime_test*/TestStatus -b cime_integration_test -g Experimental
