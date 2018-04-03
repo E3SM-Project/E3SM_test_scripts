6a40e5f5ca4e98b1e68e910a5beaf1334608efcf
Azamat Mametjanov <azamat@mcs.anl.gov>
2018-04-03 18:25:08 +0000

    Switch jenkins/anvil_cime_integration.sh to use PET_PS config

--- jenkins/anvil_cime_integration.sh
+++ jenkins/anvil_cime_integration.sh
@@ -11,6 +11,6 @@ git clone git@github.com:ESMCI/cime.git
 
 /bin/rm -rf /lcrc/group/acme/acmetest/acme_scratch/*cime_test*
 
-./cime/scripts/create_test --machine anvil PET_Ln9.ne30_oECv3_ICG.A_WCYCL1850S -t cime_test
+./cime/scripts/create_test --machine anvil PET_Ln9_PS.ne30_oECv3_ICG.A_WCYCL1850S -t cime_test
 
 ./cime/scripts/Tools/wait_for_tests /lcrc/group/acme/acmetest/acme_scratch/*cime_test*/TestStatus -b cime_integration_test -g Experimental
