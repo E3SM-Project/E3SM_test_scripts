9bc668da00d01bfd710c6af2a30e85dc25d1a65c
Wade Burgess <pwburge@sandia.gov>
2018-04-17 14:52:55 -0600

    More fixes for theta jenkins

--- jenkins/theta_hi_res.sh
+++ jenkins/theta_hi_res.sh
@@ -5,6 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=theta
 source $SCRIPTROOT/util/setup_common.sh
 
-DATE_STAMP=$(date "+%Y-%m-%d_%H%M%S")
-
-$FORCE_REPO_PATH/cime/scripts/Tools/jenkins_generic_job -j 4 --submit-to-cdash -t e3sm_hi_res >> THETA_JENKINS_$DATE_STAMP 2>&1
+$RUNSCRIPT -j 4 -t e3sm_hi_res
--- jenkins/theta_next.sh
+++ jenkins/theta_next.sh
@@ -5,6 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=theta
 source $SCRIPTROOT/util/setup_common.sh
 
-DATE_STAMP=$(date "+%Y-%m-%d_%H%M%S")
-
-$FORCE_REPO_PATH/cime/scripts/Tools/jenkins_generic_job -j 4 --submit-to-cdash -t e3sm_integration >& THETA_JENKINS_$DATE_STAMP || echo moving_on
+$RUNSCRIPT -j 4 -t e3sm_integration
