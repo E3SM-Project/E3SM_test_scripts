48d80c01cfe89122b4d8d7ddce587b710fd5c49a
James Foucar <jgfouca@sandia.gov>
2018-05-09 16:43:40 -0500

    Use new anvil scratch root

--- util/anvil_postrun.sh
+++ util/anvil_postrun.sh
@@ -1,4 +1,4 @@
 
 # .nc files producted by E3SM on blues/anvil seem to be locked-down, regardless of umask
 # so we must force the issue here.
-chmod -R g+rwX /lcrc/group/acme/$USER/acme_scratch
+chmod -R g+rwX /lcrc/group/acme/$USER/acme_scratch/anvil
