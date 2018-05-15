7853899baa2004903b94836924a7afbb6aea34ed
James Foucar <jgfouca@sandia.gov>
2018-05-15 14:03:31 -0600

    Cannot skip postrun

--- jenkins/anvil_prod.sh
+++ jenkins/anvil_prod.sh
@@ -5,6 +5,9 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=anvil
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 4 -t e3sm_prod -O master --baseline-compare=yes
+exit_code=0
+$RUNSCRIPT -j 4 -t e3sm_prod -O master --baseline-compare=yes || exit_code=1
 
 source $SCRIPTROOT/util/anvil_postrun.sh
+
+exit $exit_code
