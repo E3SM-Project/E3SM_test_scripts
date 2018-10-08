8b44ec6a302efd7e2f1d21a05b6a7616b867263c
James Foucar <jgfouca@sandia.gov>
2018-10-08 15:26:10 -0600

    Turn down parallelism for cori hi-res and prod
    I'm going to try to keep these jobs on the interactive nodes

--- jenkins/cori_prod.sh
+++ jenkins/cori_prod.sh
@@ -5,4 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=cori-knl
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 4 -t e3sm_prod -O master --baseline-compare=yes
+$RUNSCRIPT -j 2 -t e3sm_prod -O master --baseline-compare=yes
