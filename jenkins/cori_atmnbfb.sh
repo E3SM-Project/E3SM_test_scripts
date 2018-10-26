ca7409bb9d7ee13c1d1addf0d7733e91636fa9d1
James Foucar <jgfouca@sandia.gov>
2018-10-26 12:37:26 -0600

    Add custom python module load for atmnbfb test

--- jenkins/cori_atmnbfb.sh
+++ jenkins/cori_atmnbfb.sh
@@ -5,4 +5,5 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=cori-knl
 source $SCRIPTROOT/util/setup_common.sh
 
+module load python/2.7-anaconda-5.2
 $RUNSCRIPT -j 2 -t e3sm_atm_nbfb -O master --baseline-compare=yes
