260c156a4e4932061e40e7b3db1859b97e88df50
Azamat Mametjanov <azamat@mcs.anl.gov>
2018-12-07 21:47:34 +0000

    Add ClimateEnergy_3 PROJECT env-var

--- jenkins/theta_next.sh
+++ jenkins/theta_next.sh
@@ -3,6 +3,7 @@
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=theta
+export PROJECT=ClimateEnergy_3
 source $SCRIPTROOT/util/setup_common.sh
 
 $RUNSCRIPT -j 4 -t e3sm_integration
