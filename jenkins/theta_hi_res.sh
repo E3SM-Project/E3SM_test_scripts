eb9538b59e21d2d14c864150970bf02b98f2414b
Azamat Mametjanov <azamat@mcs.anl.gov>
2018-12-12 20:35:59 +0000

    Add CHARGE_ACCOUNT for Theta

--- jenkins/theta_hi_res.sh
+++ jenkins/theta_hi_res.sh
@@ -4,6 +4,7 @@
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=theta
 export PROJECT=ClimateEnergy_3
+export CHARGE_ACCOUNT=ClimateEnergy_2
 source $SCRIPTROOT/util/setup_common.sh
 
 $RUNSCRIPT -j 4 -t e3sm_hi_res
