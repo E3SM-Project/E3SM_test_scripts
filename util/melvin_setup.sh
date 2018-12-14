cc143b59ee77aa3a219fa265f7d3787cfa1ac124
James Foucar <jgfouca@sandia.gov>
2018-12-14 12:05:08 -0700

    Add cmake to melvin config

--- util/melvin_setup.sh
+++ util/melvin_setup.sh
@@ -3,7 +3,7 @@ if [ -z "$SEMS_MODULEFILES_ROOT" ]; then
     source /projects/sems/modulefiles/utils/sems-modules-init.sh
 fi
 
-module load sems-python/2.7.9 sems-git sems-pylint
+module load sems-python/2.7.9 sems-git sems-pylint sems-cmake/3.12.2
 
 export http_proxy=http://sonproxy.sandia.gov:80
 export RSYNC_PROXY=sonproxy.sandia.gov:80
