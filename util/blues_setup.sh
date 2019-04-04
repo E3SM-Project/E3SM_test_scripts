9d58ba1e41c0e5ae941315ad438023b76fa0d7f5
James Foucar <jgfouca@sandia.gov>
2019-04-04 14:16:10 -0600

    Shouldn't need to do so much setup for anvil
    Now that my login scripts are sourcing /etc/profile

--- util/blues_setup.sh
+++ util/blues_setup.sh
@@ -1,6 +1,2 @@
 
-source /home/software/spack-0.10.1/opt/spack/linux-centos7-x86_64/gcc-4.8.5/lmod-7.4.9-ic63herzfgw5u3na5mdtvp3nwxy6oj2z/lmod/lmod/init/sh
-
-export MODULEPATH=/blues/gpfs/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/mvapich2/2.3.1-dtbb6xk/intel/18.0.4:/blues/gpfs/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/intel/18.0.4:/blues/gpfs/home/software/spack-0.10.1/share/spack/lmod/linux-centos7-x86_64/Core:/soft/bebop/modulefiles:/opt/cray/modulefiles:/opt/cray/craype/default/modulefiles:/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/Core:/software/centos7/modulefiles
-
 module load git/2.13.0 python/2.7.15-7utbzum
