618cdf45312f5bc783a574180a4898d93cf6d48e
James Foucar <jgfouca@sandia.gov>
2019-04-04 12:07:51 -0600

    Update anvil/blues setup

--- util/blues_setup.sh
+++ util/blues_setup.sh
@@ -1,7 +1,6 @@
 
-# Set up python
-export PATH=/software/python-gnu-2.7.5/bin:$PATH
-export LD_LIBRARY_PATH=/software/python-gnu-2.7.5/lib:$LD_LIBRARY_PATH
+source /home/software/spack-0.10.1/opt/spack/linux-centos7-x86_64/gcc-4.8.5/lmod-7.4.9-ic63herzfgw5u3na5mdtvp3nwxy6oj2z/lmod/lmod/init/sh
 
-# load git
-soft add +git-2.5.0
+export MODULEPATH=/blues/gpfs/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/mvapich2/2.3.1-dtbb6xk/intel/18.0.4:/blues/gpfs/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/intel/18.0.4:/blues/gpfs/home/software/spack-0.10.1/share/spack/lmod/linux-centos7-x86_64/Core:/soft/bebop/modulefiles:/opt/cray/modulefiles:/opt/cray/craype/default/modulefiles:/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/Core:/software/centos7/modulefiles
+
+module load git/2.13.0 python/2.7.15-7utbzum
