2576dac4a786b93c536b466484f0f384988ce79e
James Foucar <jgfouca@sandia.gov>
2019-04-08 12:38:19 -0600

    Add cmake to anvil setup

--- util/blues_setup.sh
+++ util/blues_setup.sh
@@ -3,4 +3,4 @@ source /home/software/spack-0.10.1/opt/spack/linux-centos7-x86_64/gcc-4.8.5/lmod
 
 export MODULEPATH=/blues/gpfs/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/mvapich2/2.3.1-dtbb6xk/intel/18.0.4:/blues/gpfs/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/intel/18.0.4:/blues/gpfs/home/software/spack-0.10.1/share/spack/lmod/linux-centos7-x86_64/Core:/soft/bebop/modulefiles:/opt/cray/modulefiles:/opt/cray/craype/default/modulefiles:/software/centos7/spack/share/spack/lmod/linux-centos7-x86_64/Core:/software/centos7/modulefiles
 
-module load git/2.13.0 python/2.7.15-7utbzum
+module load git/2.13.0 python/2.7.15-7utbzum cmake/3.12.2-4zllpyo
