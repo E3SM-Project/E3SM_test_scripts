9c61ac0c95b089a35d5170f62b62de38ddd75662
Azamat Mametjanov <azamat@mcs.anl.gov>
2019-04-18 11:44:48 -0500

    Update Anvil intel compiler module

--- util/blues_setup.sh
+++ util/blues_setup.sh
@@ -2,4 +2,4 @@
 source /home/software/spack-0.10.1/opt/spack/linux-centos7-x86_64/gcc-4.8.5/lmod-7.4.9-ic63herzfgw5u3na5mdtvp3nwxy6oj2z/lmod/lmod/init/sh
 export MODULEPATH=/blues/gpfs/software/centos7/spack-0.12.1/share/spack/lmod/linux-centos7-x86_64/Core
 
-module load intel/17.0.4-nymb5wx cmake
+module load intel/17.0.4-74uvhji cmake
