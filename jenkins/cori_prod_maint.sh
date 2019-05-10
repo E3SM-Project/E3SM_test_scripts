455c10c763619520fff1e84c41c7b9b2114533d8
James Foucar <jgfouca@sandia.gov>
2019-05-10 13:27:46 -0600

    New cori maint script
    Needs to update submodules manually.

---
+++ jenkins/cori_prod_maint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=cori-knl
+source $SCRIPTROOT/util/setup_common.sh
+
+git submodule update --init
+$RUNSCRIPT -j 2 -t e3sm_prod -O master --baseline-compare=yes
