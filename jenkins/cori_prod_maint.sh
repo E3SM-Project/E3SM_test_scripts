be8cb8383eb3239daae5f61d6328f459f776ac2c
James Foucar <jgfouca@sandia.gov>
2019-05-10 13:30:03 -0600

    Fix for cori_prod_maint

--- jenkins/cori_prod_maint.sh
+++ jenkins/cori_prod_maint.sh
@@ -5,5 +5,6 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=cori-knl
 source $SCRIPTROOT/util/setup_common.sh
 
-git submodule update --init
+cd $E3SMREPO && git submodule update --init
+cd -
 $RUNSCRIPT -j 2 -t e3sm_prod -O master --baseline-compare=yes
