c21a28e7cf21f54e639d322f896f5c4f921c2a40
wadeburgess <pwburge@sandia.gov>
2019-08-20 13:26:50 -0600

    remove -j 4 and pgi as the compiler

--- jenkins/compy_next.sh
+++ jenkins/compy_next.sh
@@ -5,4 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 4
+$RUNSCRIPT --compiler pgi
