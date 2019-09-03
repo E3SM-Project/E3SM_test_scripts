0653becc6bda1dfbd14ba79128f44645923f2584
wadeburgess <pwburge@sandia.gov>
2019-09-03 08:48:46 -0600

    Update and rename chama_extra_coverage.sh to extra_coverage.sh

--- jenkins/extra_coverage.sh
+++ jenkins/extra_coverage.sh
@@ -2,7 +2,7 @@
 
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=sandiatoss3
+export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
 $RUNSCRIPT -j4 -t e3sm_extra_coverage --baseline-compare=no
