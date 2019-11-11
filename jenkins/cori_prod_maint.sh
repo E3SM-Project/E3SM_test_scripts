39b5d50cb52171226cf92b94e6847704f0bcdc91
Joseph H Kennedy <me@jhkennedy.org>
2019-11-11 10:41:30 -0900

    Python 2 relapse on Cori for maint tests

--- jenkins/cori_prod_maint.sh
+++ jenkins/cori_prod_maint.sh
@@ -5,6 +5,11 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=cori-knl
 source $SCRIPTROOT/util/setup_common.sh
 
+# deactivate cime_env for python 2
+conda deactivate
+module load python/2.7-anaconda-5.2
+
+
 cd $E3SMREPO && git submodule update --init
 cd -
 $RUNSCRIPT -j 2 -t e3sm_prod --baseline-compare=yes
