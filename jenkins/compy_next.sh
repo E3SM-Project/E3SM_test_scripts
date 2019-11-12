761ed5e421a9f69bc11c4412d3cd41ad005c1f56
Azamat Mametjanov <azamat@mcs.anl.gov>
2019-11-12 07:56:35 -0800

    Add Compy baseline path

--- jenkins/compy_next.sh
+++ jenkins/compy_next.sh
@@ -5,6 +5,6 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT --compiler pgi -O master --baseline-compare=yes
+$RUNSCRIPT --compiler pgi -O master --baseline-compare=yes -B /compyfs/e3sm_baselines/pgi/
 
 chmod -R g+rwX /compyfs/$USER/e3sm_scratch
