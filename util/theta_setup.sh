95bda1cf425eb7bb39a004ed9aee382a6309ae38
Azamat Mametjanov <azamat@mcs.anl.gov>
2020-01-07 18:53:37 +0000

    Update project name on Theta

--- util/theta_setup.sh
+++ util/theta_setup.sh
@@ -1,4 +1,4 @@
-export PROJECT=ClimateEnergy_3
-export CHARGE_ACCOUNT=ClimateEnergy_3
+export PROJECT=ClimateEnergy_4
+export CHARGE_ACCOUNT=ClimateEnergy_4
 
 source /lus/theta-fs0/projects/ccsm/acme/tools/e3sm-unified/load_latest_cime_env.sh
