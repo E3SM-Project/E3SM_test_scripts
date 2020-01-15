ba4fd8dced75d7ac8d975524702f3331e0458b7c
wadeburgess <pwburge@sandia.gov>
2020-01-15 14:51:18 -0700

    Create chama_homme.sh

---
+++ jenkins/chama_homme.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=sandiatoss3
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT
