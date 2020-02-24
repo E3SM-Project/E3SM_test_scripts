450e3c84f14934510e5bca99428fae62b44d419d
wadeburgess <pwburge@sandia.gov>
2020-02-24 10:40:51 -0700

    Update compy_bgcprod_maint.sh

--- jenkins/compy_bgcprod_maint.sh
+++ jenkins/compy_bgcprod_maint.sh
@@ -6,6 +6,10 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
+# deactivate cime_env for python 2
+conda deactivate
+module load python/2.7-anaconda-5.2
+
 $RUNSCRIPT --compiler pgi --baseline-compare=yes -t e3sm_bgcprod
 
 chmod -R g+rwX /compyfs/$USER/e3sm_scratch
