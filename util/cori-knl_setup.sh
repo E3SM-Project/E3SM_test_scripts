8a5a016a89c878e60ece44600a7d6f55960589d5
wadeburgess <pwburge@sandia.gov>
2020-03-27 12:12:10 -0600

    Update cori-knl_setup.sh

--- util/cori-knl_setup.sh
+++ util/cori-knl_setup.sh
@@ -1,2 +1,2 @@
 module load git
-source /global/project/projectdirs/acme/software/anaconda_envs/load_latest_cime_env.sh
+source /global/project/projectdirs/e3sm/software/anaconda_envs/load_latest_cime_env.sh
