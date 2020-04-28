33eba7d6c2532b63a429296dca27805bad778c96
James Foucar <jgfouca@sandia.gov>
2020-04-28 13:42:23 -0600

    Fix up CIME nightly testing

--- util/run_cime_tests.sh
+++ util/run_cime_tests.sh
@@ -1,7 +1,11 @@
 
 cd $SCRIPTROOT/..
+
+/bin/rm -rf E3SM/cime
+mv cime E3SM/cime
+
 /bin/rm -rf build
 mkdir build
 cd build
-cmake ../cime/scripts/tests
+cmake ../E3SM/cime/scripts/tests
 ctest -D NightlyStart -D NightlyTest -D NightlySubmit
