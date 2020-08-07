7257b8ef6bf4d02929ba6504afc85c65853c95cd
James Foucar <jgfouca@sandia.gov>
2020-08-06 20:15:48 -0400

    Another fix for skybridge cime testing

--- jenkins/skybridge_cime.sh
+++ jenkins/skybridge_cime.sh
@@ -3,7 +3,6 @@
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=sandiatoss3
-export FORCE_REPO_PATH=cime
 source $SCRIPTROOT/util/setup_common.sh
 
 source $SCRIPTROOT/util/run_cime_tests.sh
