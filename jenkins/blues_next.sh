b182e8eb8768a339fd0316e58b9a8bb6ea1dc8f9
Azamat Mametjanov <azamat@mcs.anl.gov>
2020-08-14 14:36:49 -0500

    Add e3sm_gpu test suite for blues

--- jenkins/blues_next.sh
+++ jenkins/blues_next.sh
@@ -3,6 +3,8 @@
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=blues
+
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 4
+$RUNSCRIPT -j 4 -t e3sm_gpu
+
