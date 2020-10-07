a9aea49a7da6d5225ab4a361f27d2ef6fe863ceb
wadeburgess <pwburge@sandia.gov>
2020-10-07 14:23:27 -0600

    Create compy_next_intel.sh

---
+++ jenkins/compy_next_intel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=compy
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT --compiler intel -O master --baseline-compare=yes
+
+chmod -R g+rwX /compyfs/$USER/e3sm_scratch
