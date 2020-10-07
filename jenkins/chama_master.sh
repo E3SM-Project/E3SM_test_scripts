b2f39caa41527be6721f5cf4a17e7e17f061b4fa
wadeburgess <pwburge@sandia.gov>
2020-10-07 14:52:35 -0600

    Update chama_master.sh

--- jenkins/chama_master.sh
+++ jenkins/chama_master.sh
@@ -6,3 +6,6 @@ export CIME_MACHINE=sandiatoss3
 source $SCRIPTROOT/util/setup_common.sh
 
 $RUNSCRIPT
+
+chmod -R g+rwX /nscratch/e3sm-jenkins/acme_scratch/sandiatoss3
+chmod -R g+rwX /gpfs1/e3sm-jenkins/acme_scratch/sandiatoss3
