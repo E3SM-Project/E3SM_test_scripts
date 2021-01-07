6720e3f5e2c3fb26fed295dddaf1467a3553f4dd
Sarat Sreepathi <sarat@ornl.gov>
2021-01-06 20:53:46 -0500

    PACE-Anvil: switch acme to e3sm

--- jenkins/anvil_pace.sh
+++ jenkins/anvil_pace.sh
@@ -8,11 +8,11 @@ export CIME_MACHINE=anvil
 #source $SCRIPTROOT/util/setup_common.sh
 
 # Performance archive directory on this platform
-export PERF_ARCHIVE_DIR=/lcrc/group/acme/performance_archive
+export PERF_ARCHIVE_DIR=/lcrc/group/e3sm/performance_archive
 # Archives of old performance data on this platform
 export OLD_PERF_ARCHIVE_DIR=${PERF_ARCHIVE_DIR}/../OLD_PERF
 # Following tag is used by the Perl script in naming the perf. archive directory
-export PROJ_SPACE_TAG=acme
+export PROJ_SPACE_TAG=e3sm
 
 module load requests/2.20.1
 source $SCRIPTROOT/util/pace_archive.sh
