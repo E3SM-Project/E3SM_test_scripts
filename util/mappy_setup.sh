a58515496df5aa708b09278b42c44d9f3fffacfd
James Foucar <jgfouca@sandia.gov>
2021-05-14 12:56:52 -0600

    Update to use py3 except for master

--- util/mappy_setup.sh
+++ util/mappy_setup.sh
@@ -3,7 +3,14 @@ if [ -z "$SEMS_MODULEFILES_ROOT" ]; then
     source /projects/sems/modulefiles/utils/sems-modules-init.sh
 fi
 
-module load sems-python/2.7.9 sems-git sems-pylint sems-cmake/3.12.2
+module load sems-git sems-cmake/3.12.2
+
+if [[ $GIT_BRANCH == *master* ]]; then
+    module load sems-python/2.7.9
+else
+    module load sems-python/3.5.2
+fi
+module load sems-pylint
 
 export http_proxy=http://wwwproxy.sandia.gov:80
 export RSYNC_PROXY=wwwproxy.sandia.gov:80
--- util/sandiatoss3_setup.sh
+++ util/sandiatoss3_setup.sh
@@ -3,7 +3,14 @@ if [ -z "$SEMS_MODULEFILES_ROOT" ]; then
     source /projects/sems/modulefiles/utils/sems-modules-init.sh
 fi
 
-module load sems-python/2.7.9 sems-git sems-pylint/1.5.4/base
+module load sems-git
+
+if [[ $GIT_BRANCH == *master* ]]; then
+    module load sems-python/2.7.9
+else
+    module load sems-python/3.5.2
+fi
+module load sems-pylint
 
 export PATH=/projects/ccsm/perl-5.10.1/bin:$PATH
 
