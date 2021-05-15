8ada4d473b41b194295bf4da33c40686fafdcb8b
Azamat Mametjanov <azamat@mcs.anl.gov>
2021-05-14 20:18:07 -0400

    Add test script for OLCF Ascent

---
+++ util/ascent_setup.sh
@@ -0,0 +1,6 @@
+
+source /sw/ascent/lmod/7.8.2/rhel7.5_4.8.5/lmod/lmod/init/sh
+
+export MODULEPATH=/sw/ascent/modulefiles/20180914/site/linux-rhel7-ppc64le/Core:/sw/ascent/modulefiles/core
+
+module load python/3.7.0 git/2.13.0 cmake/3.13.4
