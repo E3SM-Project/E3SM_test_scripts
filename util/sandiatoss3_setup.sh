d3fb3d30d87f8f5734002d8c740c9d182e4c6ded
James Foucar <jgfouca@sandia.gov>
2021-05-19 08:52:14 -0600

    Use py3 for everything now

--- util/sandiatoss3_setup.sh
+++ util/sandiatoss3_setup.sh
@@ -3,14 +3,7 @@ if [ -z "$SEMS_MODULEFILES_ROOT" ]; then
     source /projects/sems/modulefiles/utils/sems-modules-init.sh
 fi
 
-module load sems-git
-
-if [[ $GIT_BRANCH == *master* ]]; then
-    module load sems-python/2.7.9
-else
-    module load sems-python/3.5.2
-fi
-module load sems-pylint/1.5.4/base
+module load sems-git sems-python/3.5.2 sems-pylint/1.5.4/base
 
 export PATH=/projects/ccsm/perl-5.10.1/bin:$PATH
 
