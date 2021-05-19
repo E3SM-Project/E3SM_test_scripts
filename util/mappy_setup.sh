6fd9d42b224a96e798492c0b37e81fc8bd9d9716
James Foucar <jgfouca@sandia.gov>
2021-05-19 08:51:32 -0600

    Use py3 for everything now

--- util/mappy_setup.sh
+++ util/mappy_setup.sh
@@ -3,14 +3,7 @@ if [ -z "$SEMS_MODULEFILES_ROOT" ]; then
     source /projects/sems/modulefiles/utils/sems-modules-init.sh
 fi
 
-module load sems-git sems-cmake/3.12.2
-
-if [[ $GIT_BRANCH == *master* ]]; then
-    module load sems-python/2.7.9
-else
-    module load sems-python/3.5.2
-fi
-module load sems-pylint
+module load sems-git sems-cmake/3.12.2 sems-python/3.5.2 sems-pylint
 
 export http_proxy=http://wwwproxy.sandia.gov:80
 export RSYNC_PROXY=wwwproxy.sandia.gov:80
