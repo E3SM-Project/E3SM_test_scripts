94ae272099c07f6bec5cce4965e3f76ed18b6e84
James Foucar <jgfouca@sandia.gov>
2021-06-12 12:59:43 -0600

    Update Sandia proxies

--- util/mappy_setup.sh
+++ util/mappy_setup.sh
@@ -5,9 +5,9 @@ fi
 
 module load sems-git sems-cmake/3.12.2 sems-python/3.5.2 sems-pylint
 
-export http_proxy=http://wwwproxy.sandia.gov:80
-export RSYNC_PROXY=wwwproxy.sandia.gov:80
-export rsync_proxy=wwwproxy.sandia.gov:80
-export HTTPS_PROXY=http://wwwproxy.sandia.gov:80
-export https_proxy=http://wwwproxy.sandia.gov:80
-export HTTP_PROXY=http://wwwproxy.sandia.gov:80
+export http_proxy=http://proxy.sandia.gov:80
+export RSYNC_PROXY=proxy.sandia.gov:80
+export rsync_proxy=proxy.sandia.gov:80
+export HTTPS_PROXY=http://proxy.sandia.gov:80
+export https_proxy=http://proxy.sandia.gov:80
+export HTTP_PROXY=http://proxy.sandia.gov:80
--- util/sandiatoss3_setup.sh
+++ util/sandiatoss3_setup.sh
@@ -7,10 +7,10 @@ module load sems-git sems-python/3.5.2 sems-pylint/1.5.4/base
 
 export PATH=/projects/ccsm/perl-5.10.1/bin:$PATH
 
-export http_proxy=http://wwwproxy.sandia.gov:80
-export RSYNC_PROXY=wwwproxy.sandia.gov:80
-export rsync_proxy=wwwproxy.sandia.gov:80
-export HTTPS_PROXY=http://wwwproxy.sandia.gov:80
-export https_proxy=http://wwwproxy.sandia.gov:80
-export HTTP_PROXY=http://wwwproxy.sandia.gov:80
+export http_proxy=http://proxy.sandia.gov:80
+export RSYNC_PROXY=proxy.sandia.gov:80
+export rsync_proxy=proxy.sandia.gov:80
+export HTTPS_PROXY=http://proxy.sandia.gov:80
+export https_proxy=http://proxy.sandia.gov:80
+export HTTP_PROXY=http://proxy.sandia.gov:80
 
