10fa68ba80bfca324495c7cb9cb10fbf1d95f90b
James Foucar <jgfouca@sandia.gov>
2021-06-29 12:46:24 -0600

    Remove non-python setup from a number of machine setup scripts
    CIME only requires python to run. Loading anything else in these
    files is misleading because the first thing CIME does on many machines
    is a module purge, so loading cmake, compilers, or other things here has no impact
    on what CIME actually uses on the code.

--- util/anvil_setup.sh
+++ util/anvil_setup.sh
@@ -4,6 +4,4 @@ source  /lcrc/soft/climate/e3sm-unified/load_latest_cime_env.sh
 source /home/software/spack-0.10.1/opt/spack/linux-centos7-x86_64/gcc-4.8.5/lmod-7.4.9-ic63herzfgw5u3na5mdtvp3nwxy6oj2z/lmod/lmod/init/sh
 export MODULEPATH=$MODULEPATH:/software/centos7/spack-latest/share/spack/lmod/linux-centos7-x86_64/Core
 
-module load intel/17.0.0-pwabdn2 cmake/3.14.2-gvwazz3 git/2.13.0
-
 export TERM=xterm
--- util/blues_setup.sh
+++ util/blues_setup.sh
@@ -3,5 +3,3 @@ source  /lcrc/soft/climate/e3sm-unified/load_latest_cime_env.sh
 
 source /home/software/spack-0.10.1/opt/spack/linux-centos7-x86_64/gcc-4.8.5/lmod-7.4.9-ic63herzfgw5u3na5mdtvp3nwxy6oj2z/lmod/lmod/init/sh
 export MODULEPATH=/software/centos7/spack-latest/share/spack/lmod/linux-centos7-x86_64/Core:/soft/bebop/modulefiles
-
-module load cmake/3.14.2-gvwazz3 git/2.13.0
