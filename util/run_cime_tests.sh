29a0c6c7621abc4b4ace6803df2f2db33f6e30a7
James Foucar <jgfouca@sandia.gov>
2021-07-02 11:14:17 -0600

    Another try

--- util/run_cime_tests.sh
+++ util/run_cime_tests.sh
@@ -6,10 +6,8 @@ cd E3SM/cime
 # that are already up-to-date, so origin may not have recent CIME
 # changes. We need to fetch here and use https since we can't be
 # certain of the ssh key situation.
-git remote set-url origin https://github.com/ESMCI/cime.git
-git remote -v
-git fetch origin
-git reset --hard origin/master
+git fetch https://github.com/ESMCI/cime.git
+git reset --hard FETCH_HEAD
 cd -
 
 /bin/rm -rf build
