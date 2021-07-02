0ead214c887f98aca17e0224d4f2b7c415b820d3
James Foucar <jgfouca@sandia.gov>
2021-07-02 11:04:58 -0600

    Update CIME test script to ensure latest origin/master is grabbed

--- util/run_cime_tests.sh
+++ util/run_cime_tests.sh
@@ -2,6 +2,12 @@
 cd $SCRIPTROOT/..
 
 cd E3SM/cime
+# This is a subtle issue. Jenkins/Git will not fetch submodules
+# that are already up-to-date, so origin may not have recent CIME
+# changes. We need to fetch here and use https since we can't be
+# certain of the ssh key situation.
+git remote set-url origin https://github.com/ESMCI/cime.git
+git fetch
 git reset --hard origin/master
 cd -
 
