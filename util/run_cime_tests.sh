6a7cd299a7a1285f52dfe28cd944a83bb044218f
James Foucar <jgfouca@sandia.gov>
2021-07-02 14:51:01 -0600

    Go back to FETCH_HEAD

--- util/run_cime_tests.sh
+++ util/run_cime_tests.sh
@@ -1,13 +1,13 @@
 
 cd $SCRIPTROOT/..
 
-cd E3SM
+cd E3SM/cime
 # This is a subtle issue. Jenkins/Git will not fetch submodules
 # that are already up-to-date, so origin may not have recent CIME
 # changes. We need to fetch here and use https since we can't be
 # certain of the ssh key situation.
-/bin/rm -rf cime
-git clone https://github.com/ESMCI/cime.git
+git fetch https://github.com/ESMCI/cime.git
+git reset --hard FETCH_HEAD
 cd -
 
 /bin/rm -rf build
