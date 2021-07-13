9f57bf5525144b8b47ad884256d1a49e4b88c8ab
James Foucar <jgfouca@sandia.gov>
2021-07-13 17:11:21 -0600

    Use newer pythons on sandia machines

--- util/sandiatoss3_setup.sh
+++ util/sandiatoss3_setup.sh
@@ -3,8 +3,9 @@ if [ -z "$SEMS_MODULEFILES_ROOT" ]; then
     source /projects/sems/modulefiles/utils/sems-modules-init.sh
 fi
 
-module load sems-git sems-python/3.5.2 sems-pylint/1.5.4/base
+module load sems-git
 
+export PATH=/ascldap/users/jgfouca/packages/Python-3.8.5/bin:$PATH
 export PATH=/projects/ccsm/perl-5.10.1/bin:$PATH
 
 export http_proxy=http://proxy.sandia.gov:80
