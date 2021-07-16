cf68ae88b7f5f41c331b65dfc72e912ff5c083a8
James Foucar <jgfouca@sandia.gov>
2021-07-16 14:34:12 -0600

    Restore a smaller chama test. Remove melvin tests

--- jenkins/melvin_next.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xle
-
-# boiler: every script must have these three lines
-export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=melvin
-source $SCRIPTROOT/util/setup_common.sh
-
-$RUNSCRIPT -O master
