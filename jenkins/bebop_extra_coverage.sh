90ae57c371b60727e8b063d418260f54499c13f9
James Foucar <jgfouca@sandia.gov>
2021-07-22 16:11:03 -0600

    More updates

--- jenkins/bebop_extra_coverage.sh
+++ jenkins/bebop_extra_coverage.sh
@@ -5,4 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=bebop
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j4 -t e3sm_extra_coverage --baseline-compare=no
+$RUNSCRIPT -j4 -t e3sm_extra_coverage
