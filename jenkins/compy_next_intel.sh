1e39dab95712e3e1df9c8bebc3ec172ee5dd195a
James Foucar <jgfouca@sandia.gov>
2021-07-22 16:03:22 -0600

    Update baseline comparison flags

--- jenkins/compy_next_intel.sh
+++ jenkins/compy_next_intel.sh
@@ -5,6 +5,6 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT --compiler intel -t e3sm_prod -O master --baseline-compare=yes
+$RUNSCRIPT --compiler intel -t e3sm_prod -O master --baseline-compare
 
 chmod -R g+rwX /compyfs/$USER/e3sm_scratch
