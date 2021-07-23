ac47108a0b972e07ba2dfb3d0c47023ae306e09e
James Foucar <jgfouca@sandia.gov>
2021-07-23 17:12:53 -0600

    Update baseline flags

--- jenkins/chama_master.sh
+++ jenkins/chama_master.sh
@@ -5,7 +5,7 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=sandiatoss3
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -t e3sm_developer --baseline-compare=no
+$RUNSCRIPT -t e3sm_developer
 
 chmod -R g+rwX /nscratch/e3sm-jenkins/acme_scratch/sandiatoss3
 chmod -R g+rwX /gpfs1/e3sm-jenkins/acme_scratch/sandiatoss3
