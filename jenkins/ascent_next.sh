9e38a877c674b217491bec3ab91a729862989daf
wadeburgess <pwburge@sandia.gov>
2021-07-27 07:43:12 -0600

    Update ascent_next.sh

--- jenkins/ascent_next.sh
+++ jenkins/ascent_next.sh
@@ -5,4 +5,7 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=ascent
 source $SCRIPTROOT/util/setup_common.sh
 
+export http_proxy=http://proxy.ccs.ornl.gov:3128/ 
+export https_proxy=https://proxy.ccs.ornl.gov:3128/
+
 $RUNSCRIPT -j 4 -t e3sm_developer
