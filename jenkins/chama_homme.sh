ae782d8ee2e4bcbb200e022287c31f8eec2e0faf
James Foucar <jgfouca@sandia.gov>
2021-07-28 13:21:56 -0600

    Update Homme tests

--- jenkins/chama_homme.sh
+++ jenkins/chama_homme.sh
@@ -5,4 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=sandiatoss3
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -t homme_integration
+$RUNSCRIPT -t homme_integration --baseline-compare
