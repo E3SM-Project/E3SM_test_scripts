18e1ce42ed93775f6468baa815033820a98ea397
Azamat Mametjanov <azamat@mcs.anl.gov>
2021-08-10 22:36:50 -0500

    Add test script for Swing, which replaces Blues

---
+++ jenkins/swing_next.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=swing
+
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -j 4 -t e3sm_gpu
+
---
+++ util/swing_setup.sh
@@ -0,0 +1,3 @@
+source /gpfs/fs1/soft/swing/spack/opt/spack/linux-ubuntu20.04-x86_64/gcc-9.3.0/lmod-8.3-5tuyfdb/lmod/lmod/init/sh
+
+export MODULEPATH=/gpfs/fs1/soft/swing/spack/share/spack/lmod/linux-ubuntu20.04-x86_64/Core:/gpfs/fs1/soft/swing/manual/modulefiles
