f91f4e973e91a51efca6e5265123fec570fb21ed
James Foucar <jgfouca@sandia.gov>
2021-11-04 11:29:52 -0600

    Add scream_v1 testing to scream tests

--- jenkins/mappy_scream.sh
+++ jenkins/mappy_scream.sh
@@ -5,4 +5,13 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=mappy
 source $SCRIPTROOT/util/setup_common.sh
 
+declare -i fails=0
+set +e
 $RUNSCRIPT -t e3sm_scream --baseline-compare $@
+if [[ $? != 0 ]]; then fails=$fails+1; fi
+$RUNSCRIPT -t e3sm_scream_v1 --compiler=gnu9 --baseline-compare $@
+if [[ $? != 0 ]]; then fails=$fails+1; fi
+
+if [[ $fails > 0 ]]; then
+    exit 1
+fi
