eea21c68c10c1361bf97f7512c2810b5a6819d42
wadeburgess <pwburge@sandia.gov>
2021-11-18 08:43:43 -0700

    Update and rename skybridge_homme.sh to chrysalis_homme.sh

--- jenkins/chrysalis_homme.sh
+++ jenkins/chrysalis_homme.sh
@@ -2,7 +2,7 @@
 
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=sandiatoss3
+export CIME_MACHINE=chrysalis
 source $SCRIPTROOT/util/setup_common.sh
 
 $RUNSCRIPT -O master -t homme_integration --baseline-compare
