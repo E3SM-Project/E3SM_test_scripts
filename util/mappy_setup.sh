cc4ce77edaaa562c40f5ca3adf96be60fcd58d75
James Foucar <jgfouca@sandia.gov>
2021-12-21 12:18:51 -0700

    Fix mappy setup

--- util/mappy_setup.sh
+++ util/mappy_setup.sh
@@ -3,7 +3,8 @@ source /projects/sems/modulefiles/utils/sems-archive-modules-init.sh
 
 # We only need cmake here because mappy run cime standalone tests which
 # do not load the CIME env before running cmake
-module load sems-git/2.10.1 sems-cmake/3.19.1
+module purge
+module load sems-env sems-git/2.10.1 sems-cmake/3.19.1
 export PATH=/ascldap/users/jgfouca/packages/Python-3.8.5/bin:$PATH
 
 export http_proxy=http://proxy.sandia.gov:80
