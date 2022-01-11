06f358e059422492e422f9863a0c4f9741f360f5
Azamat Mametjanov <azamat@mcs.anl.gov>
2022-01-11 16:41:39 -0600

    Add e3sm_prod_bench test script for Chrysalis

---
+++ jenkins/chrysalis_prod_bench.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xle
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=chrysalis
+
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_prod_bench -O master --baseline-compare --check-throughput --check-memory
