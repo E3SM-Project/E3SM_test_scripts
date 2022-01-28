cfbabcfee2b27a292d0058f83d14049f8a90759b
Azamat Mametjanov <azamat@mcs.anl.gov>
2022-01-27 19:26:30 -0500

    Add OpenACC,OpenMP,CXX GPU test scripts

--- jenkins/ascent_gpuomp.sh
+++ jenkins/ascent_gpuomp.sh
@@ -5,7 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=ascent
 source $SCRIPTROOT/util/setup_common.sh
 
-#export http_proxy=http://proxy.ccs.ornl.gov:3128/ 
-#export https_proxy=https://proxy.ccs.ornl.gov:3128/
-
-$RUNSCRIPT -j 4 -t e3sm_integration $1
+$RUNSCRIPT -j 4 -t e3sm_gpuomp $1
