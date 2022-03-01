6f05df4a6427c52466c452885b56a52f3c97a8b9
Azamat Mametjanov <azamat@mcs.anl.gov>
2022-03-01 17:56:24 -0600

    Update ANL LCRC postrun script path

--- util/anvil_postrun.sh
+++ util/anvil_postrun.sh
@@ -1,4 +1,4 @@
 
 # .nc files producted by E3SM on blues/anvil seem to be locked-down, regardless of umask
 # so we must force the issue here.
-chmod -R g+rwX /lcrc/group/acme/$USER/acme_scratch/anvil
+chmod -R g+rwX /lcrc/group/e3sm/$USER/scratch
