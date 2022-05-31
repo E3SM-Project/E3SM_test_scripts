8d3eaf71c2bdca6dcc4fc0f7413fe1cedaf2cbc6
Luca Bertagna <lbertag@sandia.gov>
2022-05-31 14:57:55 -0600

    Update perlmutter scream jenkins scripts
    Change machine name, and create two separate scripts,
    one for gpu and one for cpu builds/tests.

--- jenkins/perlmutter_cpu_scream.sh
+++ jenkins/perlmutter_cpu_scream.sh
@@ -2,9 +2,9 @@
 
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=perlmutter
+export CIME_MACHINE=pm-cpu
 export SCREAM_MACHINE=$CIME_MACHINE
 source $SCRIPTROOT/util/setup_common.sh
 
 declare -i fails=0
-$RUNSCRIPT -t e3sm_scream_v1_medres --compiler=gnugpu
+$RUNSCRIPT -t e3sm_scream_v1_medres --compiler=gnu
