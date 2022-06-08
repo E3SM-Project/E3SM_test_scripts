666428af70c81146119cb69dd1eec8b7fe1a4a31
Azamat Mametjanov <azamat@mcs.anl.gov>
2022-06-07 22:16:21 -0500

    Run Chrysalis tests with small debug-queue PEs

--- jenkins/chrysalis_next.sh
+++ jenkins/chrysalis_next.sh
@@ -5,4 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=chrysalis
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -O master --baseline-compare
+$RUNSCRIPT -O master --baseline-compare --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml
