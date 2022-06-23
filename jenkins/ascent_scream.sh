b96261fd055ae775d575cdbff961f5e5b5a204fd
James Foucar <jgfouca@sandia.gov>
2022-06-23 14:22:13 -0600

    scream wants gnugpu compiler

--- jenkins/ascent_scream.sh
+++ jenkins/ascent_scream.sh
@@ -5,4 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=ascent
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 4 -t e3sm_scream_v1 || exit 0
+$RUNSCRIPT -j 4 -t e3sm_scream_v1 --compiler=gnugpu || exit 0
