c51a7b85e9ef5f2a149779eaf05b09c6d3a16c88
Azamat Mametjanov <azamat@mcs.anl.gov>
2022-07-14 16:29:47 -0400

    Fix CIME_MACHINE typo for Crusher

--- jenkins/crusher_next.sh
+++ jenkins/crusher_next.sh
@@ -2,7 +2,7 @@
 
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=ascent
+export CIME_MACHINE=crusher
 source $SCRIPTROOT/util/setup_common.sh
 
 $RUNSCRIPT -j 4 -t cime_tiny $1 || exit 0
