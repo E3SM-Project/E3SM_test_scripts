59d3fa712fefc430d5405b4c6db8ad3ea2d59e83
Azamat Mametjanov <azamat@mcs.anl.gov>
2023-02-10 19:09:36 -0500

    Add a script for e3sm_gpucxx with crayclanggpu on Crusher

--- jenkins/crusher_gpucxx.sh
+++ jenkins/crusher_gpucxx.sh
@@ -2,7 +2,7 @@
 
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=ascent
+export CIME_MACHINE=crusher
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 4 -t e3sm_integration $1
+$RUNSCRIPT -j 4 -t e3sm_gpucxx --compiler=crayclanggpu -O master --baseline-compare || exit 0
--- jenkins/crusher_next.sh
+++ jenkins/crusher_next.sh
@@ -5,4 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=crusher
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 4 -t e3sm_integration $1 || exit 0
+$RUNSCRIPT -j 4 -t e3sm_integration $1
