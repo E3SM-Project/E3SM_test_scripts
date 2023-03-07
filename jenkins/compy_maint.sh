f9ee0aa2f98164c9c3c6e08388559452d7c7e389
wadeburgess <pwburge@sandia.gov>
2023-03-07 13:13:01 -0700

    Update compy_maint.sh

--- jenkins/compy_maint.sh
+++ jenkins/compy_maint.sh
@@ -5,10 +5,6 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
-# deactivate cime_env for python 2
-conda deactivate
-module load python/2.7.9
-
 $RUNSCRIPT --compiler intel --baseline-compare=yes -t e3sm_prod
 
 chmod -R g+rwX /compyfs/$USER/e3sm_scratch
