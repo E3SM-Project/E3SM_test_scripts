ded43236e78089915a7391f8e0fb1aaa55c57e27
wadeburgess <pwburge@sandia.gov>
2023-03-07 12:23:29 -0700

    Create compy_maint.sh

---
+++ jenkins/compy_maint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=compy
+source $SCRIPTROOT/util/setup_common.sh
+
+# deactivate cime_env for python 2
+conda deactivate
+module load python/2.7.9
+
+$RUNSCRIPT --compiler intel --baseline-compare=yes -t e3sm_prod
+
+chmod -R g+rwX /compyfs/$USER/e3sm_scratch
