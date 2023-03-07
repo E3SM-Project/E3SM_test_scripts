26a39fe3f71c3ef9c5f38d2b51f26c0ff9d85b22
James Foucar <jgfouca@sandia.gov>
2023-03-07 13:36:45 -0700

    Dont use old API

--- jenkins/compy_maint.sh
+++ jenkins/compy_maint.sh
@@ -5,6 +5,6 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT --compiler intel --baseline-compare=yes -t e3sm_prod
+$RUNSCRIPT --compiler intel --baseline-compare -t e3sm_prod
 
 chmod -R g+rwX /compyfs/$USER/e3sm_scratch
