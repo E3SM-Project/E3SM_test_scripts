dcf9c95710a29ca91642e7622567153a64524b11
wadeburgess <pwburge@sandia.gov>
2023-05-10 15:42:24 -0400

    Delete theta_hi_res.sh

--- jenkins/theta_hi_res.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xle
-
-# boiler: every script must have these three lines
-export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=theta
-source $SCRIPTROOT/util/setup_common.sh
-
-$RUNSCRIPT -j 4 -t e3sm_hi_res
