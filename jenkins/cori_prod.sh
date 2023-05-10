0d95759fd2cc921490beb8ab6a2551be6f9b674e
wadeburgess <pwburge@sandia.gov>
2023-05-10 15:20:14 -0400

    Delete cori_prod.sh

--- jenkins/cori_prod.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xle
-
-# boiler: every script must have these three lines
-export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=cori-knl
-source $SCRIPTROOT/util/setup_common.sh
-
-$RUNSCRIPT -j 2 -t e3sm_prod -O master --baseline-compare
