73768aa9fc887ddf18c395f2ecf0cfb8fc0bf489
wadeburgess <pwburge@sandia.gov>
2023-05-10 14:42:18 -0400

    Update chrysalis_prod.sh
    make this not a login script

--- jenkins/chrysalis_prod.sh
+++ jenkins/chrysalis_prod.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xle
+#!/bin/bash -xe
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=chrysalis
