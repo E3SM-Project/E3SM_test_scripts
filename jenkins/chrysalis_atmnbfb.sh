720bca3e7f8dfa3951d353015e848c2162eaa8f6
James Foucar <jgfouca@sandia.gov>
2023-05-22 15:11:46 -0600

    Add ignore memleak to chrysalis nbfb

--- jenkins/chrysalis_atmnbfb.sh
+++ jenkins/chrysalis_atmnbfb.sh
@@ -5,4 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=chrysalis
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 4 -t e3sm_atm_nbfb -O master --baseline-compare
+$RUNSCRIPT -j 4 -t e3sm_atm_nbfb -O master --baseline-compare --ignore-memleak
