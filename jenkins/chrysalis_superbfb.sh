2fb47913da13ee5e7a397803c7bb76454a0f0600
Azamat Mametjanov <azamat@mcs.anl.gov>
2023-05-24 17:04:35 -0500

    Add e3sm_superbfb test script for Chrysalis

---
+++ jenkins/chrysalis_superbfb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=chrysalis
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_superbfb --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml
+
