e1cfc1227073d4899bf357aacdd788760fcd3ba2
Azamat Mametjanov <azamat@mcs.anl.gov>
2023-05-24 18:03:27 -0500

    Add e3sm_superbfb test script for pm-cpu+next+intel

---
+++ jenkins/perlmutter_cpu_next_superbfb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=pm-cpu
+export SCREAM_MACHINE=$CIME_MACHINE
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_superbfb --compiler=intel
+
