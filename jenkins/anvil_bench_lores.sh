335e6b4482e069b20fbdab9138968981d3d2a198
Azamat Mametjanov <azamat@mcs.anl.gov>
2023-10-23 17:40:04 -0500

    Add e3sm_bench_lores test script for Anvil+intel+next

---
+++ jenkins/anvil_bench_lores.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=anvil
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_bench_lores --compiler=intel -O master --baseline-compare --check-throughput --check-memory --save-timing
+
+source $SCRIPTROOT/util/anvil_postrun.sh
