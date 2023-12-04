b42be279b044d3c9b0d37edaf8ca5a658768f5df
James Foucar <jgfouca@sandia.gov>
2023-12-04 10:48:53 -0700

    Add baseline checking to scream perlmutter jobs

--- jenkins/perlmutter_gpu_scream.sh
+++ jenkins/perlmutter_gpu_scream.sh
@@ -7,9 +7,9 @@ export SCREAM_MACHINE=$CIME_MACHINE
 source $SCRIPTROOT/util/setup_common.sh
 
 exit_code=0
-$RUNSCRIPT -t e3sm_scream_v1 --compiler=gnugpu || exit_code=1
+$RUNSCRIPT -t e3sm_scream_v1 --compiler=gnugpu --baseline-compare || exit_code=1
 
-$RUNSCRIPT -t e3sm_scream_v1_long --compiler=gnugpu || exit_code=1
+$RUNSCRIPT -t e3sm_scream_v1_long --compiler=gnugpu --baseline-compare || exit_code=1
 
 exit $exit_code
 
