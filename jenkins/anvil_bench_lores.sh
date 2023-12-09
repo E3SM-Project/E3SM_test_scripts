dbbd895e9af88a7dc620d9ddc067b9478d519bd7
Azamat Mametjanov <azamat@mcs.anl.gov>
2023-12-08 19:32:59 -0600

    Ignore NML diffs in benchmark jobs

--- jenkins/anvil_bench_lores.sh
+++ jenkins/anvil_bench_lores.sh
@@ -5,6 +5,6 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=anvil
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -t e3sm_bench_lores --compiler=intel -O master --baseline-compare --check-throughput --check-memory --save-timing
+$RUNSCRIPT -t e3sm_bench_lores --compiler=intel -O master --baseline-compare --check-throughput --check-memory --save-timing --ignore-namelists
 
 source $SCRIPTROOT/util/anvil_postrun.sh
