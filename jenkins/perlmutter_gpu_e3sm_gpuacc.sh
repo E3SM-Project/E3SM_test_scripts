9888ef6af312c54dbd781937f81b56ec399ade09
Azamat Mametjanov <azamat@mcs.anl.gov>
2023-12-08 20:07:32 -0600

    Add test scripts for e3sm_gpucxx and e3sm_gpuacc on pm-gpu

---
+++ jenkins/perlmutter_gpu_e3sm_gpuacc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=pm-gpu
+export SCREAM_MACHINE=$CIME_MACHINE
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_gpuacc --compiler=nvidiagpu
