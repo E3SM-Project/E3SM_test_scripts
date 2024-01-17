5ab87863891e18161af443c305a0a6836305e792
James Foucar <jgfouca@sandia.gov>
2024-01-17 10:41:14 -0700

    New scream frontier job

---
+++ jenkins/frontier_gpu_scream.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=frontier-scream-gpu
+export SCREAM_MACHINE=$CIME_MACHINE
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_scream_v1_medres --compiler=crayclang-scream
---
+++ util/frontier-scream-gpu_setup.sh
@@ -0,0 +1,1 @@
+source /usr/share/lmod/lmod/init/sh
