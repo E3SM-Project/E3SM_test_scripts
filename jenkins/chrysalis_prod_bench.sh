4bc563cb55202cf6699aac89b9eebee3b9ad829e
Azamat Mametjanov <azamat@mcs.anl.gov>
2024-04-04 16:14:54 -0500

    Add jenkins-id to prevent removal by e3sm_prod jenkins job

--- jenkins/chrysalis_prod_bench.sh
+++ jenkins/chrysalis_prod_bench.sh
@@ -4,4 +4,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=chrysalis
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -t e3sm_prod_bench -b master $RUNSCRIPT_FLAGS --baseline-compare --check-throughput --check-memory --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml --save-timing --ignore-namelists --ignore-diffs
+$RUNSCRIPT -t e3sm_prod_bench -b master $RUNSCRIPT_FLAGS --baseline-compare --check-throughput --check-memory --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml --save-timing --ignore-namelists --ignore-diffs --jenkins-id JNPBench
