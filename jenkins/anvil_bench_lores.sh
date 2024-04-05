fe4c00cefb7b84649afec9c7ae806a9c79bcb410
Azamat Mametjanov <azamat@mcs.anl.gov>
2024-04-05 15:36:27 -0500

    Revert to `-O master`
    `-b master` overrides branch name in cdash build name.
    `-O master` overrides branch name in baseline comparisons.
    Run with default branch, compare with master, report for next.

--- jenkins/anvil_bench_lores.sh
+++ jenkins/anvil_bench_lores.sh
@@ -5,6 +5,6 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=anvil
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -j 2 -t e3sm_bench_lores --compiler=intel -b master --baseline-compare --check-throughput --check-memory --save-timing --ignore-namelists --ignore-diffs
+$RUNSCRIPT -j 2 -t e3sm_bench_lores --compiler=intel -O master --baseline-compare --check-throughput --check-memory --save-timing --ignore-namelists --ignore-diffs
 
 source $SCRIPTROOT/util/anvil_postrun.sh
