e22e81efebbe40a52ff14b06f5d03de181237541
Wade Burgess <pwburge@sandia.gov>
2024-04-30 11:54:48 -0400

    Update chrysalis_landice.sh

--- jenkins/chrysalis_landice.sh
+++ jenkins/chrysalis_landice.sh
@@ -5,10 +5,4 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=chrysalis
 source $SCRIPTROOT/util/setup_common.sh
 
-#chrysalis next
-#$RUNSCRIPT -O master $RUNSCRIPT_FLAGS --baseline-compare --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml
-
-#chrysalis atmnbfb
-#$RUNSCRIPT -j 4 -t e3sm_atm_nbfb $RUNSCRIPT_FLAGS -O master --baseline-compare --ignore-memleak --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml
-
-$RUNSCRIPT -O master -t e3sm_landice_developer $RUNSCRIPT_FLAGS --baseline-compare --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml
+$RUNSCRIPT -O master -t e3sm_landice_developer $RUNSCRIPT_FLAGS --baseline-compare --ignore-memleak --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml
