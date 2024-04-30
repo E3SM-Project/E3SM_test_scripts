6626a285ecf19c69cb7fd37544fc04039d8ae982
Wade Burgess <pwburge@sandia.gov>
2024-04-30 07:29:50 -0400

    Create perlmutter_cpu_landice.sh

---
+++ jenkins/perlmutter_cpu_landice.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=pm-cpu
+export SCREAM_MACHINE=$CIME_MACHINE
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_landice_developer --compiler=gnu -O master --baseline-compare
