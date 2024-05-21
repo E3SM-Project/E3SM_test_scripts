6d6d4552df03019eee5e6c07f98525822c112c0f
Wade Burgess <pwburge@sandia.gov>
2024-05-21 15:22:24 -0400

    Update compy_maint.sh to specify python version

--- jenkins/compy_maint.sh
+++ jenkins/compy_maint.sh
@@ -5,6 +5,7 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
+module load python/3.7.3
 $RUNSCRIPT --compiler intel --baseline-compare -t e3sm_prod
 
 chmod -R g+rwX /compyfs/$USER/e3sm_scratch
