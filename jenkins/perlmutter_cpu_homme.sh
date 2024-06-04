d9ec755f18e9d1262b698c792c70ffb35ebd2d9b
Wade Burgess <pwburge@sandia.gov>
2024-06-04 17:19:40 -0400

    Update perlmutter_cpu_homme.sh

--- jenkins/perlmutter_cpu_homme.sh
+++ jenkins/perlmutter_cpu_homme.sh
@@ -6,4 +6,4 @@ export CIME_MACHINE=pm-cpu
 export SCREAM_MACHINE=$CIME_MACHINE
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -O master $RUNSCRIPT_FLAGS -t homme_integration --compiler=intel --baseline-compare
+$RUNSCRIPT -O master $RUNSCRIPT_FLAGS -t homme_integration --compiler=intel
