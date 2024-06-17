a56404d60bee437af7f714444d712a5f0639c54c
Luca Bertagna <lbertag@sandia.gov>
2024-06-17 10:38:12 -0600

    Allow gnu build to run before returning in perlmutter_cpu_homme script

--- jenkins/perlmutter_cpu_homme.sh
+++ jenkins/perlmutter_cpu_homme.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -xe
 
+exit_code=0
+
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=pm-cpu
 export SCREAM_MACHINE=$CIME_MACHINE
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -O master $RUNSCRIPT_FLAGS -t homme_integration --compiler=intel
-$RUNSCRIPT -O master $RUNSCRIPT_FLAGS -t homme_integration --compiler=gnu
+$RUNSCRIPT -O master $RUNSCRIPT_FLAGS -t homme_integration --compiler=intel || exit_code=1
+$RUNSCRIPT -O master $RUNSCRIPT_FLAGS -t homme_integration --compiler=gnu || exit_code=1
+
+return exit_code
