bdc2c9026973f4275a96f4d2c451bd0e9be83b7c
James Foucar <jgfouca@sandia.gov>
2024-09-16 14:05:08 -0600

    Fix mappy setup for rhel9

--- util/mappy_setup.sh
+++ util/mappy_setup.sh
@@ -1,11 +1,10 @@
 
-source /projects/sems/modulefiles/utils/sems-archive-modules-init.sh
+source /projects/sems/modulefiles/utils/sems-modules-init.sh
 
 # We only need cmake here because mappy run cime standalone tests which
 # do not load the CIME env before running cmake
 module purge
-module load sems-archive-env sems-archive-git/2.10.1 sems-archive-cmake/3.19.1
-export PATH=/ascldap/users/jgfouca/packages/Python-3.8.5/bin:$PATH
+module load sems-git/2.42.0 sems-cmake/3.27.9
 
 export http_proxy=http://proxy.sandia.gov:80
 export RSYNC_PROXY=proxy.sandia.gov:80
