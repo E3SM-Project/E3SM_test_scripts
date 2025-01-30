98f9d1c3d8b76d20523b9c9214f0a0510766b8ef
Azamat Mametjanov <azamat@mcs.anl.gov>
2025-01-30 21:14:41 +0000

    Move Sunspot tests to Aurora

--- jenkins/aurora_next.sh
+++ jenkins/aurora_next.sh
@@ -2,7 +2,7 @@
 
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=sunspot
+export CIME_MACHINE=aurora
 source $SCRIPTROOT/util/setup_common.sh
 
 $RUNSCRIPT -j 4 -t e3sm_integration $1
