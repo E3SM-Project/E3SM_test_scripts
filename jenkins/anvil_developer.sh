1ce07663905274451afc8ae1438020264670ebeb
Azamat Mametjanov <azamat@mcs.anl.gov>
2025-02-05 19:26:03 -0600

    Source cime_env in anvil_developer.sh

--- jenkins/anvil_developer.sh
+++ jenkins/anvil_developer.sh
@@ -5,6 +5,9 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=anvil
 source $SCRIPTROOT/util/setup_common.sh
 
+# load newer python
+source /lcrc/soft/climate/e3sm-unified/load_latest_cime_env.sh
+
 exit_code=0
 $RUNSCRIPT -j 4 -t e3sm_developer $RUNSCRIPT_FLAGS -O master --baseline-compare --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml || exit_code=1
 
