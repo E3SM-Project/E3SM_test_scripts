203b86b93eb41d4be2c853283cb9636ae2513d21
James Foucar <jgfouca@sandia.gov>
2025-02-17 14:21:09 -0700

    Update frontier machine and compiler names

--- jenkins/frontier_gpu_scream.sh
+++ jenkins/frontier_gpu_scream.sh
@@ -2,9 +2,9 @@
 
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=frontier-scream-gpu
+export CIME_MACHINE=frontier
 export SCREAM_MACHINE=$CIME_MACHINE
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=crayclang-scream -b master --jenkins-id A
-$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craygnuamdgpu -b master --jenkins-id B
+$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craycray-mphipcc -b master --jenkins-id A
+$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craygnu-hipcc -b master --jenkins-id B
--- jenkins/frontier_gpu_scream_perf.sh
+++ jenkins/frontier_gpu_scream_perf.sh
@@ -2,8 +2,8 @@
 
 # boiler: every script must have these three lines
 export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
-export CIME_MACHINE=frontier-scream-gpu
+export CIME_MACHINE=frontier
 export SCREAM_MACHINE=$CIME_MACHINE
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -t e3sm_eamxx_v1_hires --compiler=crayclang-scream -b master --baseline-compare --check-throughput --check-memory --save-timing --ignore-namelists --ignore-diffs
+$RUNSCRIPT -t e3sm_eamxx_v1_hires --compiler=craycray-mphipcc -b master --baseline-compare --check-throughput --check-memory --save-timing --ignore-namelists --ignore-diffs
