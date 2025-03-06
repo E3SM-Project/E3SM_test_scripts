a898cd77d0ebc69cdfb3afa4338a3b22d3d8390e
James Foucar <jgfouca@sandia.gov>
2025-03-06 10:30:07 -0700

    Fix frontier test. failure in one should not prevent the second one from running

--- jenkins/frontier_gpu_scream.sh
+++ jenkins/frontier_gpu_scream.sh
@@ -6,5 +6,8 @@ export CIME_MACHINE=frontier
 export SCREAM_MACHINE=$CIME_MACHINE
 source $SCRIPTROOT/util/setup_common.sh
 
-$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craycray-mphipcc -b master --jenkins-id A
-$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craygnu-hipcc -b master --jenkins-id B
+exit_code=0
+$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craycray-mphipcc -b master --jenkins-id A || exit_code=1
+$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craygnu-hipcc -b master --jenkins-id B    || exit_code=1
+
+exit $exit_code
