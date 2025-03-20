0319e02e0c68f9dd5714aaf388c36d08c389d21a
Azamat Mametjanov <azamat@mcs.anl.gov>
2025-03-20 03:06:27 +0000

    Add eamxx test script for Polaris

---
+++ jenkins/polaris_eamxx_v1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=polaris
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -j 4 -t e3sm_eamxx_v1 $1 || exit 0
