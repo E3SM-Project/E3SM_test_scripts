6b035d13b22a942be32b5617ad0dcb5949939e8e
singhbalwinder <singhbalwinder@users.noreply.github.com>
2025-04-02 13:26:42 -0700

    Update frontier_gpu_scream.sh

--- jenkins/frontier_gpu_scream.sh
+++ jenkins/frontier_gpu_scream.sh
@@ -7,7 +7,9 @@ export SCREAM_MACHINE=$CIME_MACHINE
 source $SCRIPTROOT/util/setup_common.sh
 
 exit_code=0
-$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craycray-mphipcc -b master --jenkins-id A || exit_code=1
-$RUNSCRIPT -t e3sm_eamxx_v1_medres --compiler=craygnu-hipcc -b master --jenkins-id B    || exit_code=1
+$RUNSCRIPT -t e3sm_eamxx_v1_medres     --compiler=craycray-mphipcc -b master --jenkins-id A || exit_code=1
+$RUNSCRIPT -t e3sm_eamxx_v1_medres     --compiler=craygnu-hipcc    -b master --jenkins-id B || exit_code=1
+$RUNSCRIPT -t e3sm_eamxx_mam4xx_lowres --compiler=craygnu-hipcc    -b master --jenkins-id C || exit_code=1
+
 
 exit $exit_code
