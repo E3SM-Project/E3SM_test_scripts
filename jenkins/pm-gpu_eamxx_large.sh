ee83859ddda4917d181b36dc37c94ea7a8cd5c1d
James Foucar <jgfouca@sandia.gov>
2025-08-07 09:45:05 -0600

    Add new eamxx pmgpu large scripts

---
+++ jenkins/pm-gpu_eamxx_large.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xle
+
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=pm-gpu
+export SCREAM_MACHINE=$CIME_MACHINE
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_eamxx_large --compiler=gnugpu --baseline-compare
