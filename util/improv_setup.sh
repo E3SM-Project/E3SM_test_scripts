c14c8843720ac72e250928abf709dc63231fab13
Azamat Mametjanov <azamat@mcs.anl.gov>
2025-10-08 14:49:44 -0500

    Load specific python in Improv setup script

--- util/improv_setup.sh
+++ util/improv_setup.sh
@@ -1,2 +1,4 @@
 #
-module load python
+source /gpfs/fs1/soft/chrysalis/spack/opt/spack/linux-centos8-x86_64/gcc-9.3.0/lmod-8.3-5be73rg/lmod/lmod/init/sh
+module --force purge
+module load python/3.11.6
