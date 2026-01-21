3a283cc1a02c8e8a98d663b7d35954efdbd60966
Azamat Mametjanov <azamat@mcs.anl.gov>
2026-01-21 14:50:47 -0600

    Add e3sm_prod on chrysalis with oneapi-ifx compiler

---
+++ jenkins/chrysalis_prod_oneapi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+# boiler: every script must have these three lines
+export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+export CIME_MACHINE=chrysalis
+source $SCRIPTROOT/util/setup_common.sh
+
+$RUNSCRIPT -t e3sm_prod --compiler oneapi-ifx -O master $RUNSCRIPT_FLAGS --baseline-compare --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml
