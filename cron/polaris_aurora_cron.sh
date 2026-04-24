e09494a28d88bdbbd496f8aad78329412e44190f
Youngsung Kim <kimy@ornl.gov>
2026-04-24 15:31:26 +0000

    adjust aurora path

--- cron/polaris_aurora_cron.sh
+++ cron/polaris_aurora_cron.sh
@@ -3,7 +3,7 @@
 HERE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 # top directory of all Polaris cronjob work
-export POLARIS_CRON_ROOT="/lus/flare/projects/$PROJECT/$USER/polaris_scratch/cron"
+export POLARIS_CRON_ROOT="/lus/flare/projects/E3SM_Dec/$USER/polaris_scratch/cron"
 
 mkdir -p $POLARIS_CRON_ROOT
 
