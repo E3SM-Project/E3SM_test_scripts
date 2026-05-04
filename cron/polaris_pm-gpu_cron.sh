1b44664f5ff2cb772d8e1d6110bb858349a09e88
Youngsung Kim <youngsung.kim.act2@gmail.com>
2026-05-04 13:36:07 -0400

    remove slurm memory restriction on pm-gpu

--- cron/polaris_pm-gpu_cron.sh
+++ cron/polaris_pm-gpu_cron.sh
@@ -7,5 +7,10 @@ export POLARIS_CRON_ROOT="$PSCRATCH/polaris_scratch/cron/pm-gpu"
 
 mkdir -p $POLARIS_CRON_ROOT
 
+if [[ ! -z "${SLURM_MEM_PER_CPU}" ]]; then
+unset SLURM_MEM_PER_CPU
+unset SLURM_OPEN_MODE
+fi
+
 # launch polaris cronjob
 exec bash $HERE/polaris_cron.sh -m pm-gpu
