0e840de04b6de873bdc22cd82d8343805328e430
James Foucar <jgfouca@sandia.gov>
2026-05-19 15:31:10 -0600

    Use older e3sm unified env for prod maint onchrsy

--- jenkins/chrysalis_prod_maint.sh
+++ jenkins/chrysalis_prod_maint.sh
@@ -4,4 +4,7 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=chrysalis
 source $SCRIPTROOT/util/setup_common.sh
 
+# Need an older e3sm unified due to maint-3.0 using older CIME
+source /lcrc/soft/climate/e3sm-unified/load_e3sm_unified_1.10.0_chrysalis.sh
+
 $RUNSCRIPT -t e3sm_prod $RUNSCRIPT_FLAGS --baseline-compare --pes-file $E3SMREPO/cime_config/testmods_dirs/config_pes_tests.xml
