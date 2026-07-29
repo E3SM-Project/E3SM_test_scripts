aa4ba798499cb5c9abd332fc4ef1433c6b2310d4
Youngsung Kim <youngsung.kim.act2@gmail.com>
2026-07-29 11:32:24 -0400

    add env variables for accessing outer network from Frontier node

--- cron/polaris_frontier_cron.sh
+++ cron/polaris_frontier_cron.sh
@@ -2,8 +2,16 @@
 
 HERE=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
+source /etc/bash.bashrc
+
 # top directory of all Polaris cronjob work
-export POLARIS_CRON_ROOT="/lustre/orion/cli115/proj-shared/${USER}/polaris_scratch/cron"
+export POLARIS_CRON_ROOT="/lustre/orion/cli115/proj-shared/$USER/polaris_scratch/cron"
+
+export all_proxy=socks://proxy.ccs.ornl.gov:3128/
+export ftp_proxy=ftp://proxy.ccs.ornl.gov:3128/
+export http_proxy=http://proxy.ccs.ornl.gov:3128/
+export https_proxy=http://proxy.ccs.ornl.gov:3128/
+export no_proxy='localhost,127.0.0.0/8,*.ccs.ornl.gov'
 
 mkdir -p $POLARIS_CRON_ROOT
 
