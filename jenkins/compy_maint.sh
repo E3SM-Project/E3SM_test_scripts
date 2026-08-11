7e2165459c8c6d2591032c3ee26d706b012b872e
James Foucar <jgfouca@sandia.gov>
2026-08-11 12:32:45 -0600

    Fix compy maint py version

--- jenkins/compy_maint.sh
+++ jenkins/compy_maint.sh
@@ -5,7 +5,9 @@ export SCRIPTROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 export CIME_MACHINE=compy
 source $SCRIPTROOT/util/setup_common.sh
 
-#module load python/3.7.3
+# Can't use too new of a python for older CIMEs
+module unload python
+module load python/3.10.19
 $RUNSCRIPT --compiler intel --baseline-compare -t e3sm_prod
 
 chmod -R g+rwX /compyfs/$USER/e3sm_scratch
