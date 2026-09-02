adc530790fc536d014508c9757f76cf929baf41b
Azamat Mametjanov <azamat@mcs.anl.gov>
2026-09-02 14:15:20 -0500

    Unload pre-loaded python on Chrysalis, use cime_env's python

--- util/chrysalis_setup.sh
+++ util/chrysalis_setup.sh
@@ -1,7 +1,9 @@
 #
-source  /lcrc/soft/climate/e3sm-unified/load_latest_cime_env.sh
-
 source /gpfs/fs1/soft/chrysalis/spack/opt/spack/linux-centos8-x86_64/gcc-9.3.0/lmod-8.3-5be73rg/lmod/lmod/init/sh
+
+# unload any previously loaded python, use python from load_latest_cime_env.sh
+module unload python
+source /lcrc/soft/climate/e3sm-unified/load_latest_cime_env.sh
 
 export PROJECT=e3sm
 
